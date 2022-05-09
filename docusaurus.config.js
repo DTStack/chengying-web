8005f8c36884cd5a09f39663c5900f1d45f447a1
jiangbo@dtstack.com <wosiguwozai15427>
2022-05-09 20:51:31 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'chengyingDocs/intro',
             position: 'left',
             label: 'Docs',
           },
